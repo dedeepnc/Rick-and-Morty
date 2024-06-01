6770b8a8ab7431f6e83c33ea6bf0e330d8f6cba6
dedeepnc <dedee.pnc@gmail.com>
2024-06-01 13:54:02 +1000

    Fix path for globals.css and integrate Google Analytics GA4

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
+import '../styles/globals.css';
 
 const GA_MEASUREMENT_ID = 'G-FFDC17DYLF';
 
